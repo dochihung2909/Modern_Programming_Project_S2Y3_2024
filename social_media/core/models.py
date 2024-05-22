374cf0334573c2610d529f8b88c8cef8d91850df
nguyenHung <nguyenhung1572003@gmail.com>
2024-05-22 17:49:32 +0700

    mode-create-user,post,interaction

--- social_media/core/models.py
+++ social_media/core/models.py
@@ -1,3 +1,65 @@
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    avatar = models.ImageField(null=True)
+
+
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True, null=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(BaseModel):
+    content = RichTextField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
+
+
+class Tag(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ItemBase(BaseModel):
+    tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        abstract = True
+
+
+class Interaction(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user_id} - {self.post_id}'
+
+    class Meta:
+        abstract = True
+
+
+class Comment(Interaction):
+    content = models.CharField(max_length=255)
+
+
+class Like(Interaction):
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+
+# class LikeComment(BaseModel):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     comment = models.ForeignKey(User, on_delete=models.CASCADE)
+
