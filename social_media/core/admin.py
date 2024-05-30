0fe6308c8b597b0e1ec1820693c5b2088c01de03
nguyenHung <nguyenhung1572003@gmail.com>
2024-05-30 19:20:58 +0700

    ⚡ Alter models comment, like ➕ Create models Role, LikeComment, LikePost ➕ Test add, update, delete Admin site

--- social_media/core/admin.py
+++ social_media/core/admin.py
@@ -1,10 +1,96 @@
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django.contrib import admin
-from core.models import User, Post, Tag, Comment
+from django import forms
+from django.db.models import Count
+from django.template.response import TemplateResponse
+from django.urls import path
+from django.utils.html import strip_tags
+from core.models import User, Post, Tag, Comment, Like, LikePost, LikeComment, Role
 
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Post)
-admin.site.register(Tag)
-# admin.site.register(LikePost)
-# admin.site.register(LikeComment)
-admin.site.register(Comment)
+
+class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget)
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'created_date', 'short_content', 'user_id']
+    list_filter = ['user_id']
+    search_fields = ['content']
+    form = PostForm
+
+    def short_content(self, obj):
+        content = strip_tags(obj.content)
+        return content[:50] + '...' if len(content) > 50 else content
+
+    short_content.short_description = 'Content'
+
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'created_date', 'name']
+    list_filter = ['name']
+    search_fields = ['name']
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username']
+    list_filter = ['role_id']
+    search_fields = ['username']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_id', 'post_id', 'short_content']
+    list_filter = ['user_id']
+    search_fields = ['content']
+
+    def short_content(self, obj):
+        content = strip_tags(obj.content)
+        return content[:50] + '...' if len(content) > 50 else content
+
+    short_content.short_description = 'Content'
+
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+class LikePostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_id', 'post_id']
+    list_filter = ['user_id', 'post_id']
+
+
+class LikeCommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_id', 'comment_id']
+    list_filter = ['user_id', 'comment_id']
+
+
+class SocialMediaAppAdminSite(admin.AdminSite):
+    site_header = 'HỆ THỐNG MẠNG XÃ HỘI CHO CỰU SINH VIÊN'
+
+    def get_urls(self):
+        return [path('post-stats/', self.stats_view)] + super().get_urls()
+
+    def stats_view(self, request):
+        c = Post.objects.filter(active=True).count()
+        stats = Post.objects \
+            .annotate(user_count=Count('id')) \
+            .values('id', 'content', 'user_count')
+        post_stats = User.objects.annotate(c=Count('post__id')).values('id', 'username', 'c')
+        return TemplateResponse(request, 'admin/stats.html', {
+            "post_stats": post_stats
+        })
+
+
+admin_site = SocialMediaAppAdminSite(name='socialMedia')
+
+admin_site.register(User, UserAdmin)
+admin_site.register(Post, PostAdmin)
+admin_site.register(Tag)
+admin_site.register(LikePost, LikePostAdmin)
+admin_site.register(LikeComment, LikeCommentAdmin)
+admin_site.register(Comment, CommentAdmin)
+admin_site.register(Role, RoleAdmin)
+admin_site.register(Like, LikeAdmin)
--- social_media/core/models.py
+++ social_media/core/models.py
@@ -3,10 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
-    avatar = models.ImageField(null=True)
-
-
 class BaseModel(models.Model):
     created_date = models.DateTimeField(auto_now_add=True, null=True)
     updated_date = models.DateTimeField(auto_now=True, null=True)
@@ -16,12 +12,17 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class Post(BaseModel):
-    content = RichTextField(max_length=50)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class Role(BaseModel):
+    id = models.PositiveIntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.content
+        return f'{self.id} - {self.name}'
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='user/%Y/%m', null=True)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
 
 
 class Tag(BaseModel):
@@ -32,34 +33,63 @@ class Tag(BaseModel):
 
 
 class ItemBase(BaseModel):
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     class Meta:
         abstract = True
+
+
+class Post(ItemBase):
+    content = RichTextField()
+    image = models.ImageField(upload_to='posts/%Y/%m', null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user_id} - {self.content[:50] + '...' if len(self.content) > 50 else self.content}'
+
+
+class ItemBase(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Comment(ItemBase):
+    content = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.user_id} - {self.content[:50]}'
+
+
+class Like(BaseModel):
+    name = models.CharField(max_length=50, null=True)
+    image = models.ImageField(upload_to='likes/%Y/%m', null=True, blank=True)
 
 
 class Interaction(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'{self.user_id} - {self.post_id}'
+    like = models.ForeignKey(Like, on_delete=models.CASCADE)
 
     class Meta:
         abstract = True
 
 
-class Comment(Interaction):
-    content = models.CharField(max_length=255)
-
-
-class Like(Interaction):
+class LikePost(Interaction):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('user', 'post')
 
 
-# class LikeComment(BaseModel):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     comment = models.ForeignKey(User, on_delete=models.CASCADE)
+class LikeComment(Interaction):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'comment')
+
+
+
+
 
